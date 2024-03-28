002d259dcd8c7cbf918fa4c17eae4a940a4c64d7
Seragors <Seragorsk@gmail.com>
2024-03-28 21:44:51 +0200

    fix comment

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -8,10 +8,20 @@ import org.hibernate.Session;
 import redis.RedisService;
 
 import java.util.List;
+import java.util.Scanner;
 
 import static java.util.Objects.nonNull;
 
 public class Menu {
+    private static final int EXIT = 0;
+    private static final int ALL_CITY = 1;
+    private static final int CITY_ID = 2;
+    private static final String MENU_SELECT = """
+            1. All_City
+            2. City_Id               
+            0. Exit
+            """;
+    private static boolean isRun = true;
     static final HibenrnateUtil CONNECTION_DATABASE = new HibenrnateUtil();
     static final Session currentSession = CONNECTION_DATABASE.getCurrentSession();
     static final CityDAO cityDAO = new CityDAO(City.class, currentSession.getSessionFactory());
@@ -19,24 +29,38 @@ public class Menu {
     static final RedisUtil REDIS_UTIL = new RedisUtil();
     static final RedisClient redisClient = REDIS_UTIL.getRedisClient();
     static final RedisService transformator = new RedisService();
+    Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        List<City> all = cityDAO.fetchData();
-        transformator.pushToRedis(transformator.transformData(all), redisClient, mapper);
-        List<Integer> ids = List.of(3, 2545, 123, 4, 189, 89, 3458, 1189, 10, 102);
+    public void open() {
+        while (isRun) {
+            System.out.println(MENU_SELECT);
+            int number = scanner.nextInt();
+            if (number == ALL_CITY) {
+                System.out.println(ALL_CITY);
+                List<City> all = cityDAO.fetchData();
+                transformator.pushToRedis(transformator.transformData(all), redisClient, mapper);
+                List<Integer> ids = List.of(3, 2545, 123, 4, 189, 89, 3458, 1189, 10, 102);
+                long startRedis = System.currentTimeMillis();
+                REDIS_UTIL.RedisData(ids, redisClient, mapper);
+                long stopRedis = System.currentTimeMillis();
 
-        long startRedis = System.currentTimeMillis();
-        REDIS_UTIL.RedisData(ids, redisClient, mapper);
-        long stopRedis = System.currentTimeMillis();
+                long startMysql = System.currentTimeMillis();
+                CONNECTION_DATABASE.MySQLData(ids, cityDAO);
+                long stopMysql = System.currentTimeMillis();
 
-        long startMysql = System.currentTimeMillis();
-        CONNECTION_DATABASE.MySQLData(ids, cityDAO);
-        long stopMysql = System.currentTimeMillis();
-
-        System.out.printf("%s:\t%d ms\n", "Redis", (stopRedis - startRedis));
-        System.out.printf("%s:\t%d ms\n", "MySQL", (stopMysql - startMysql));
-
-        shutdown();
+                System.out.printf("%s:\t%d ms\n", "Redis", (stopRedis - startRedis));
+                System.out.printf("%s:\t%d ms\n", "MySQL", (stopMysql - startMysql));
+            } else if (number == CITY_ID) {
+                System.out.println(CITY_ID);
+                System.out.println("Enter the database ID");
+                int numberCityId = scanner.nextInt();
+                cityDAO.findById(numberCityId);
+            } else if (number == EXIT) {
+                shutdown();
+                isRun = false;
+                System.out.println("By!");
+            }
+        }
     }
 
     private static void shutdown() {
--- src/main/java/dao/EntityDAO.java
+++ src/main/java/dao/EntityDAO.java
@@ -1,11 +1,13 @@
 package dao;
 
+import entity.City;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,14 +45,23 @@ public abstract class EntityDAO<T> {
         int totalCount = getTotalCount().intValue();
         Transaction transaction = getCurrentSession().beginTransaction();
         for (int i = 0; i < totalCount; i += step) {
-            resultList.addAll(getItems(i,step));
+            resultList.addAll(getItems(i, step));
         }
         transaction.commit();
         return resultList;
     }
 
-    public T getById(final int id){
-        Optional<T> index = Optional.of(getCurrentSession().get(base,id));
+    public T getById(final int id) {
+        Optional<T> index = Optional.of(getCurrentSession().get(base, id));
         return index.orElse(null);
+    }
+
+    public List<T> findById(Integer id) {
+        Transaction transaction = getCurrentSession().beginTransaction();
+        Query<T> query = getCurrentSession().createQuery("SELECT c FROM City c WHERE c.id = :id", base);
+        query.setParameter("id", id);
+        City city = (City) query.getSingleResult();
+        transaction.commit();
+        return (List<T>) List.of(city);
     }
 }
